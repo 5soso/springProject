2935dbf1115a143eceba7801a6f0d1f016c19bb4
5soso <apple_1114@naver.com>
2023-12-11 12:00:57 +0900

    20231211_1200 xml 연습

---
+++ src/main/java/com/spring/sExam/exam1211/Test3VO.java
@@ -0,0 +1,80 @@
+package com.spring.sExam.exam1211;
+
+public class Test3VO {
+	private String name;
+	private int kor; 
+	private int eng; 
+	private int mat; 
+	private int tot; 
+	private double avg; 
+	private String grade;
+	
+	public Test3VO() {} //class 만드는 곳에서는 setter를 넣을지, 생성자로 넣을지 모르기때문에 기본생성자를 기본으로 하나 준다. 
+
+	public Test3VO(String name, int kor, int eng, int mat, int tot, double avg, String grade) {
+		this.name = name;
+		this.kor = kor;
+		this.eng = eng;
+		this.mat = mat;
+		this.tot = tot;
+		this.avg = avg;
+		this.grade = grade;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getKor() {
+		return kor;
+	}
+
+	public void setKor(int kor) {
+		this.kor = kor;
+	}
+
+	public int getEng() {
+		return eng;
+	}
+
+	public void setEng(int eng) {
+		this.eng = eng;
+	}
+
+	public int getMat() {
+		return mat;
+	}
+
+	public void setMat(int mat) {
+		this.mat = mat;
+	}
+
+	public int getTot() {
+		return tot;
+	}
+
+	public void setTot(int tot) {
+		this.tot = tot;
+	}
+
+	public double getAvg() {
+		return avg;
+	}
+
+	public void setAvg(double avg) {
+		this.avg = avg;
+	}
+
+	public String getGrade() {
+		return grade;
+	}
+
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+	
+}
